a0cf23ff6dbff7bf04a85deb7975d0abaca7694c
jairo_sandoval <jairo.sandoval.velasquez@gmail.com>
2020-10-24 17:26:24 -0400

    project ad

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Index = () =>{
+    return(
+        <div>
+            <h1>hello word</h1>
+            <label>correo electronico</label>
+            <input type="email" ></input><br/>
+            <label>Contraseña</label>
+            <input type="pasword" ></input><br/>
+            <button>iniciar sesion</button>
+        </div>
+    )
+}
+
+export default Index
